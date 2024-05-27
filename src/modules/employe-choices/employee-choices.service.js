6a90b55820fb1abbd3b4a692601257894d556efd
Jeba Anika <anikanur98@gmail.com>
2024-05-27 21:45:49 +0600

    feat: employee choice creation with choice date

--- src/modules/employe-choices/employee-choices.service.js
+++ src/modules/employe-choices/employee-choices.service.js
@@ -2,14 +2,14 @@ const pool = require("../../db")
 
 const createEmployeeChoice = async (data) => {
     const employee = await pool.query(`SELECT id FROM employees WHERE userid =$1`, [data?.userid])
-    const lunchOption = await pool.query(`SELECT id FROM lunch_options WHERE id =$1`, [data?.lunchoptionid])
+    const lunchOption = await pool.query(`SELECT * FROM lunch_options WHERE id =$1`, [data?.lunchoptionid])
     if (employee.rows.length < 1) {
         throw new Error('No matching employee in database!')
     }
     if (lunchOption.rows.length < 1) {
         throw new Error('No matching lunch option in database!')
     }
-    const result = await pool.query(`INSERT INTO employee_choices (userid ,lunchoptionid , employeeid) VALUES ($1, $2, $3) RETURNING *`, [data?.userid, data?.lunchoptionid, employee.rows[0].id])
+    const result = await pool.query(`INSERT INTO employee_choices (userid ,lunchoptionid , employeeid,choice_date) VALUES ($1, $2, $3,$4) RETURNING *`, [data?.userid, data?.lunchoptionid, employee.rows[0].id, lunchOption.rows[0].date])
     return result.rows[0]
 }
 

361bcb1a70d88d1dae73c8bb3a75f763514b2f7c
Jeba Anika <anikanur98@gmail.com>
2024-05-28 00:08:20 +0600

    feat: created authorizeAdmin and authorizeEmployee middlewares

---
+++ src/middlewares/authorizeAdmin.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const pool = require('../db');
+
+const authorizeAdmin = async (req, res, next) => {
+    const token = req.headers['authorization']
+    if (!token) {
+        return res.status(403).json({ success: false, message: 'No token provided', err: '' });
+    }
+
+    try {
+        const { userId, role } = jwt.decode(token, config.jwt_secret)
+        const isUserExists = await pool.query(`SELECT * FROM users WHERE id=$1`, [userId])
+        if (isUserExists.rows.length > 0) {
+            if (role === 'admin') {
+                next()
+            } else {
+                throw new Error('User is not authorized!')
+            }
+        } else {
+            throw new Error('User does not exists!')
+        }
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message || 'User is not authorized!',
+            error: err
+        })
+    }
+}
+
+module.exports = authorizeAdmin

54369804fce8fb6359d95a3ba0a11a340a0f16f8
Jeba Anika <anikanur98@gmail.com>
2024-05-29 22:10:13 +0600

    fix(api): added authorization middleware to the lunch options endpoints

--- src/modules/lunch-options/lunch-options.route.js
+++ src/modules/lunch-options/lunch-options.route.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const { createMenuController, getAllMenusController, getSingleMenuController, updateMenuController, deleteMenuController } = require('./lunch-options.controller')
+const authorizeEmployee = require('../../middlewares/authorizeEmployee')
+const authorizeAdmin = require('../../middlewares/authorizeAdmin')
+const authorizeUser = require('../../middlewares/authorizeUser')
 const router = express.Router()
 
 
-router.post('/create-menu', createMenuController)
-router.get('/:lunchOptionId', getSingleMenuController)
-router.put('/:lunchOptionId', updateMenuController)
-router.delete('/:lunchOptionId', deleteMenuController)
-router.get('/', getAllMenusController)
+router.post('/create-menu', authorizeAdmin, createMenuController)
+router.get('/:lunchOptionId', authorizeUser, getSingleMenuController)
+router.put('/:lunchOptionId', authorizeAdmin, updateMenuController)
+router.delete('/:lunchOptionId', authorizeAdmin, deleteMenuController)
+router.get('/', authorizeUser, getAllMenusController)
 
 module.exports = router

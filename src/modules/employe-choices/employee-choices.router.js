4d20677f79d86516d99eff58f7971659e2ff1cfc
Jeba Anika <anikanur98@gmail.com>
2024-05-29 22:13:49 +0600

    fix(api): added authorization middleware to employee choices endpoints

--- src/modules/employe-choices/employee-choices.router.js
+++ src/modules/employe-choices/employee-choices.router.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const { createEmployeeChoiceController, getAllEmployeeChoicesController, getLunchChoiceOfSingleEmployeeController, updateLunchChoiceOfSingleEmployeeController } = require('./employee-choices.controller')
+const authorizeEmployee = require('../../middlewares/authorizeEmployee')
+const authorizeUser = require('../../middlewares/authorizeUser')
+const authorizeAdmin = require('../../middlewares/authorizeAdmin')
 const router = express.Router()
 
-router.post('/', createEmployeeChoiceController)
-router.get('/:employeeId', getLunchChoiceOfSingleEmployeeController)
-router.put('/:choiceId', updateLunchChoiceOfSingleEmployeeController)
-router.get('/', getAllEmployeeChoicesController)
+router.post('/', authorizeEmployee, createEmployeeChoiceController)
+router.get('/:employeeId', authorizeUser, getLunchChoiceOfSingleEmployeeController)
+router.put('/:choiceId', authorizeAdmin, updateLunchChoiceOfSingleEmployeeController)
+router.get('/', authorizeAdmin, getAllEmployeeChoicesController)
 
 
 module.exports = router

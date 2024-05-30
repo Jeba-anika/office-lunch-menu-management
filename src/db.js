75f21fa9a882b9fdae1b4e38fda686edf0405bdc
Jeba Anika <anikanur98@gmail.com>
2024-05-30 11:35:28 +0600

    chore: Vercel database url added

--- src/db.js
+++ src/db.js
@@ -1,10 +1,9 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'office-lunch-menu',
-    password: '5343',
-    port: 5432,
+    connectionString: process.env.POSTGRES_URL,
 })
 
 module.exports = pool
